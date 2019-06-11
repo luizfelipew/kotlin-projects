b754aeef096ed8e33baff3568ddc8d43a9320c66
luiz.campos <luiz.campos@sinqia.com.br>
2019-06-11 13:27:32 -0300

    First commit - importando o projeto no intellij

---
+++ pontointeligente/src/main/kotlin/com/wendt/pontointeligente/PontointeligenteApplication.kt
@@ -0,0 +1,11 @@
+package com.wendt.pontointeligente
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PontointeligenteApplication
+
+fun main(args: Array<String>) {
+	SpringApplication.run(PontointeligenteApplication::class.java, *args)
+}
